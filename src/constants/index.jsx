ad5d2d7691f15ee2a24979263863f0863a1449d1
Shair Ali <shairali@Shairs-MacBook-Pro.local>
2024-07-02 11:56:22 +0500

    updated testimonial section content

--- src/constants/index.jsx
+++ src/constants/index.jsx
@@ -36,13 +36,13 @@ export const testimonials = [
     user: "David Johnson",
     company: "Quantum Innovations",
     image: user3,
-    text: "Working with this company was a pleasure. Their attention to detail and commitment to excellence are commendable. I would highly recommend them to anyone looking for top-notch service.",
+    text: "Working with this company was a pleasure. Their attention to detail and commitment to excellence are commendable.",
   },
   {
     user: "Ronee Brown",
     company: "Fusion Dynamics",
     image: user4,
-    text: "Working with the team at XYZ Company was a game-changer for our project. Their attention to detail and innovative solutions helped us achieve our goals faster than we thought possible. We are grateful for their expertise and professionalism!",
+    text: "Working with the team at XYZ Company was a game-changer for our project. Their attention to detail and innovative solutions helped us achieve our goals faster than we thought possible.",
   },
   {
     user: "Michael Wilson",
